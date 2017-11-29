be83f53e8e5838c6f1cc52a3ba3fdcc9a79b454e
Nandor Devai <mail@devainandor.com>
2017-11-29 13:40:45 +0100

    more readable code

--- planets_moons/sketch.js
+++ planets_moons/sketch.js
@@ -26,37 +26,37 @@ function draw() {
 		pos = Math.max(Math.round(mouseX / (windowWidth / 13 - 3)) - 1, 0);
 		textSize(10);
 		noStroke();
-		for (let [j, m] of planets.planets[pos].moons.entries()) {
+		for (let [j, moon] of planets.planets[pos].moons.entries()) {
 			if (j % 2) {
 				textAlign(LEFT);
-				text(m, pos * padding + 10, 68 + j * 8);
+				text(moon, pos * padding + 10, 68 + j * 8);
 			} else {
 				textAlign(RIGHT);
-				text(m, pos * padding - 10, 68 + j * 8);
+				text(moon, pos * padding - 10, 68 + j * 8);
 			}
 		}
 	}
 }
 
-function drawLabel(p, i) {
+function drawLabel(planet, position) {
 	textFont("Rajdhani", 12);
 	textAlign(CENTER);
 	fill(WHITE);
-	text(p.name, i * padding, 40);
+	text(planet.name, position * padding, 40);
 }
 
-function drawPlanet(p, i) {
-	fill(150 + 40 * (i % 3));
+function drawPlanet(planet, position) {
+	fill(150 + 40 * (position % 3));
 	noStroke();
-	let r = Math.max(Math.round(p.diameter / 12742) * 20, 2);
-	ellipse(i * padding, 10 - r / 2, r, r);
+	let r = Math.max(Math.round(planet.diameter / 12742) * 20, 2);
+	ellipse(position * padding, 10 - r / 2, r, r);
 
-	for (let [j, m] of p.moons.entries()) {
+	for (let [i, moon] of planet.moons.entries()) {
 		stroke(WHITE);
-		if (j % 10 === 0 && j > 0) {
-			ellipse(i * padding + 0.5, j * 8 + 65.5, 2, 2);
+		if (i % 10 === 0 && i > 0) {
+			ellipse(i * padding + 0.5, i * 8 + 65.5, 2, 2);
 		} else {
-			point(i * padding, j * 8 + 65);
+			point(position * padding, i * 8 + 65);
 		}
 	}
 }
