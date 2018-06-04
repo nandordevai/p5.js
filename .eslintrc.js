0e0adc9f62ad535f60cef474ba941c938ecaeb3e
Nandor Devai <mail@devainandor.com>
2018-06-04 22:33:17 +0200

    housekeeping stuff

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: [
+    'standard',
+    'p5js',
+    'p5js/dom',
+    'p5js/sound',
+  ],
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': ['error', 'always'],
+    'no-unused-vars': 'off',
+  }
+}
